70c5a85632369a8a9497681d2fd7318d9135c836
raducham <147919614+raducham@users.noreply.github.com>
2023-11-12 17:10:21 +0100

    dodalem skrypt dodawania uzytkownikow
    Skypt tworzy admina i nadaje mu potrzebne(na razie) uprawninia. Tworzenie tabel jako domyslny uzytkownik System jest nie wskazane, traktowana sa wtedy jako super krytyczne systemowe tabele i sa problemu pozniej na dzialaniu na nich

---
+++ Baza Dancyh/skrypt_tworzacy_uzytkownikow.sql
@@ -0,0 +1,31 @@
+CREATE USER c##mr IDENTIFIED BY dbpass2;
+
+GRANT CREATE SESSION TO c##admin;
+
+
+GRANT CREATE TABLE TO c##admin;
+
+
+GRANT ALTER ANY TABLE TO c##admin;
+
+
+GRANT CREATE VIEW TO c##admin;
+
+
+GRANT CREATE PROCEDURE TO c##admin;
+
+
+GRANT CREATE TRIGGER TO c##admin;
+
+
+GRANT CREATE USER TO c##admin;
+
+
+GRANT ALTER USER TO c##admin;
+
+
+GRANT DBA TO c##admin;
+
+
+COMMIT;
+
