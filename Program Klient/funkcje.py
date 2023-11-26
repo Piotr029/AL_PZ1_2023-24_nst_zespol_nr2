d7fcb124295e858edc0c9d39b3e8b8c033141320
raducham <147919614+raducham@users.noreply.github.com>
2023-11-26 16:48:06 +0100

    Update funkcje.py do v1.1

--- Program Klient/funkcje.py
+++ Program Klient/funkcje.py
@@ -1,5 +1,10 @@
 import socket
 import pickle
+from tkinter.simpledialog import askstring
+import tkinter.messagebox
+
+VERSION = 1.1
+
 HEADER = 4
 FORMAT = 'utf-8'
 ROZLACZ = '!Out'
@@ -58,6 +63,46 @@ def dodaj_czesc(klient: socket.socket, dane_f):
     nowe_dane = odbierz_dane(klient)
     return nowe_dane
 
+def usun_czesc(klient, ui):
+    confirmed = tkinter.messagebox.askyesno("Potwierdzenie usunięcia", "Czy na pewno chcesz usunąć ten wiersz danych?")
+    if confirmed:
+        id = ui.tree.item(ui.tree.selection(),"values") [0]
+        txt = f"DEL/CZESCI/{id}"
+        msg, msg_length = zakoduj_txt(txt)
+        klient.send(msg_length)
+        klient.send(msg)
+        potwiedzenie = klient.recv(DL_POTWIERDZENIA).decode(FORMAT)
+        if potwiedzenie != OK: blad_przesylu()
+        ui.czesci = odbierz_dane(klient)
+        ui.update_tree()
+    
+def zmien_czesc(klient, ui):
+    selected_item = ui.tree.selection()
+    if selected_item:
+        # Pobranie wartości zaznaczonego elementu
+        selected_value = ui.tree.item(selected_item, 'values')
+        # Wywołanie okna dialogowego do wprowadzenia nowej wartości
+        nazwa_kolumny = ui.tree.column(ui.wybrna_kol, 'id')
+        index_kol = int(ui.wybrna_kol.lstrip("#"))
+        id = selected_value[0]
+        if nazwa_kolumny == 'Typ':  # Jeżeli kolumna to 'Typ', użyj Combobox
+            new_value = ui.show_combobox_dialog("Zmiana wartości", ui.typy)
+        else:
+            new_value = askstring("Zmiana wartości", f"Wprowadź nową zawartość dla {nazwa_kolumny}:", initialvalue=selected_value[int(index_kol-1)])
+        # Aktualizacja wartości w Treeview
+        if new_value is not None:
+            txt = f"UPDT/CZESCI/{id},{nazwa_kolumny},{new_value}"
+            msg, msg_length = zakoduj_txt(txt)
+            klient.send(msg_length)
+            klient.send(msg)
+            potwiedzenie = klient.recv(DL_POTWIERDZENIA).decode(FORMAT)
+            if potwiedzenie != OK: blad_przesylu()
+            ui.czesci = odbierz_dane(klient)
+            ui.update_tree()
+                
+    
+
+
 def pobierz_czesci(klient: socket.socket):
     txt = 'GET/CZESCI/ALL'
     msg, msg_length = zakoduj_txt(txt)
@@ -69,5 +114,3 @@ def pobierz_czesci(klient: socket.socket):
         print('[MSG-OK] Przesłano Prawidlowo')
         dane = odbierz_dane(klient)
     return dane
-    
-    
